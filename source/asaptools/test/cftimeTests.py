dfab6de87fd46b576765e02e05007a0a9c9c5377
Kevin Paul <kpaul@ucar.edu>
2015-06-23 13:25:54 -0600

    Adding CFTime and corresponding unit tests modules (minimal so far)

---
+++ source/asaptools/test/cftimeTests.py
@@ -0,0 +1,21 @@
+"""
+Unit tests for the CFTime module
+
+Copyright 2015, University Corporation for Atmospheric Research
+See LICENSE.txt for details
+"""
+
+import unittest
+
+from asaptools import cftime
+
+
+class Test(unittest.TestCase):
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
